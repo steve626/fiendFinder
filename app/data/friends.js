909a81430bb12f7197bab682a37866bf4c020060
Steve Valasek <svalasek@gmail.com>
2018-05-01 11:54:43 -0700

    working app, deploying to heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,14 +5,14 @@ var friendArray = [
     "name": "Glenn",
     "photo": "http://metalnexus.net/wp-content/uploads/2017/05/17621927_10154757876493071_9048929851751619829_o-1024x1024.jpg",
     "scores": [
-        2,
-        1,
-        1,
-        1,
-        4,
         5,
         1,
-        2,
+        1,
+        5,
+        1,
+        1,
+        1,
+        1,
         1,
         1
     ]
@@ -22,19 +22,33 @@ var friendArray = [
     "photo": "https://2fast2die.com/wp-content/uploads/2015/12/misfits-jerry-3-600x400.jpg",
     "scores": [
     1,
-    2,
     3,
-    4,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3,
+    3
+]
+},
+{
+    "name": "Doyle",
+    "photo": "http://www.thevintagevegans.com/wp-content/uploads/2016/05/featured-image-8.jpg",
+    "scores": [
+    1,
+    5,
     5,
     1,
-    2,
-    3,
-    4,
+    5,
+    5,
+    5,
+    5,
+    5,
     5
 ]
 }
-
-
 ];
 
 module.exports = friendArray;
